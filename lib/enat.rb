bc2e87d21d848ddade8921139dda10a5e1850c43
Hari Gopal <harigopal1@gmail.com>
2013-07-10 12:07:55 +0530

    Cleaning up base require file.

--- lib/enat.rb
+++ lib/enat.rb
@@ -1,9 +1,8 @@
 # Gems
-require "active_support/time"
+require 'active_support/time'
 
 # Local
-require_relative "enat/version"
-require_relative "enat/error"
-require_relative "enat/enat"
+require_relative 'enat/version'
+require_relative 'enat/enat'
 
 include Enat
